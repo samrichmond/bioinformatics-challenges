cfa699d0fe149a5be251f6f2757a3aa12b54fd4f
samrichmond <75299183+samrichmond@users.noreply.github.com>
2026-06-06 03:35:11 +0000

    update problem rabbits

--- Problem_Rabbits_and_Recurrence_Relations/problem_output.py
+++ Problem_Rabbits_and_Recurrence_Relations/problem_output.py
@@ -5,6 +5,16 @@ with open('problem_input.txt') as f:
 #n, k = map(int, f.readline().split()) an alternative code that I want to remember
 
 #Fn = Fn-1 + Fn-2
+#n = months
+#k = pairs of offspring
+#1 pair to start, each newborn pair takes 1 month to reach reproduction age and then 1 month to grow the babies, 
+# so in each pair's 3rd month, they have k pairs of offspring
+#F1 = 1, F2 = 1
+#Fn = Fn-1 + k(Fn-2)
 
+def rabbitpairs(n):
+    if n == 1 or n == 2: # base case!
+        return 1
+    return rabbitpairs(n-1) + k * rabbitpairs(n-2) #recursion
 
-
+print(rabbitpairs(n))
